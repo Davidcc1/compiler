5a0de608e999c2e63aee569157e8f0d311c58a0a
Davidcc1 <davidcc72@gmail.com>
2017-05-16 17:28:22 +0200

    First commit

---
+++ senzillc.tab.h
@@ -0,0 +1,96 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_SENZILLC_TAB_H_INCLUDED
+# define YY_YY_SENZILLC_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    NUMBER = 258,
+    IDENTIFIER = 259,
+    LABEL = 260,
+    IF = 261,
+    WHILE = 262,
+    SKIP = 263,
+    THEN = 264,
+    ELSE = 265,
+    FI = 266,
+    DO = 267,
+    END = 268,
+    DOT = 269,
+    INTEGER = 270,
+    READ = 271,
+    WRITE = 272,
+    LET = 273,
+    IN = 274,
+    COMENT = 275,
+    ASSGNOP = 276,
+    LPAREN = 277,
+    RPAREN = 278,
+    STR = 279
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
+{
+#line 69 "senzillc.y" /* yacc.c:1909  */
+ 
+   int intval; /* Integer values */ 
+   char *id; /* Identifiers */ 
+   char *str;
+   struct lbs *lbls; /* For backpatching */ 
+
+#line 86 "senzillc.tab.h" /* yacc.c:1909  */
+};
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_SENZILLC_TAB_H_INCLUDED  */
